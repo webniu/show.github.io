b64677c8a9a340d918dab3317ad142201937429b
workingjob <5607715+workingjob@user.noreply.gitee.com>
2022-10-03 15:46:33 +0800

    1

---
+++ static/json/data.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+export let axzlxtjtb={}
+
+export let ayjltjtb={"code":200,"success":true,"data":[{"XH":["4","3","2","1"]},{"CCSPFWMJ":["1857","5887","3402","72"]},{"XMMC":["重油蛋糕","海绵蛋糕","天使蛋糕","慕斯蛋糕"]},{"WZPYJGXYPZMJBL":["","","14","0.51"]},{"HJ":["","","",""]},{"ZJCMJ":["456","8941","14248","5853"]},{"XMDM":["40","30","20","10"]},{"CCSPFWMJYPZMJBL":["764","2","18.311503","7"]},{"ZJCMIYPZYDMJBL":["87","95","766","74"]},{"WZPYJGXMJ":["","","247","609"]}],"msg":"操作成功"}
+export let jcwttj={"code":200,"success":true,"data":[{"CCSPFWMJ":"7018","WZPYJGXYPZMJBL":"0.5%","HJ":"0","ZJCMJ":"845","CCSPFWMJYPZMJBL":"5.7%","ZJCMIYPZYDMJBL":"67%","WZPYJGXMJ":"165"}],"msg":"操作成功"}
+export let ztxm={"code":200,"success":true,"data":[{"XH":"1","XMMC":"重油蛋糕","SL":"266"},{"XH":"2","XMMC":"海绵蛋糕","SL":"29"},{"XH":"3","XMMC":"天使蛋糕","SL":"33"},{"XH":"4","XMMC":"慕斯蛋糕","SL":"9"}],"msg":"操作成功"}
+export let zbmclist={"code":200,"success":true,"data":[{"zbmc":"zjcmj","description":"鲜牛奶","sjzb":"1","zbxh":"1","lx":"1"},{"zbmc":"ccspfwmj","description":"高钙奶","sjzb":"1","zbxh":"2","lx":"1"},{"zbmc":"ccspfwmj","description":"酸奶","sjzb":"1","zbxh":"2","lx":"1"}],"msg":"操作成功"}
